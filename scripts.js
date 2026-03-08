33b4ed603edd5a6cead588f274f2364b60a04b6b
Adam Tonachella <adam.tonachella@gmail.com>
2026-03-08 16:14:52 -0700

    g

--- scripts.js
+++ scripts.js
@@ -1,11 +1,8 @@
 console.log(`Basic Testing Purposes`);
 
 let userSTATE;
+    userSTATE = window.prompt("What state are you from?");
+    console.log(userSTATE);
 
-userSTATE = window.prompt("What state are you from?");
-
-console.log(userSTATE);
-
-// find a better script after you learn git
 
 //  Display your notes here

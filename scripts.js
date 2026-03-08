ac0262f73e446a004e3271a8c13ae676951cc342
Adam Tonachella <adam.tonachella@gmail.com>
2026-03-08 15:23:32 -0700

    x

--- scripts.js
+++ scripts.js
@@ -1,18 +1,10 @@
-let originalConsoleLog = console.log;
-
-console.log = originalConsoleLog;
-
-delete console.log;
-
 console.log(`Basic Testing Purposes`);
 
 let userSTATE;
 
-username = window.prompt("What state are you from?");
+userSTATE = window.prompt("What state are you from?");
 
 console.log(userSTATE);
-
-
 
 
 //  Display your notes here

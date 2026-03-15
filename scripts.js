9bf7530a735f1ea3a0eddb07f7b793d9a680944e
Adam Tonachella <adam.tonachella@gmail.com>
2026-03-15 16:07:35 -0700

    k

--- scripts.js
+++ scripts.js
@@ -1,9 +1,4 @@
 console.log(`Basic Testing Purposes`);
 
-let userSTATE;
-    userSTATE = window.prompt("Our website will never share your information with third parties");
-    console.log(userSTATE);
-
-
 
 //  Display your notes here

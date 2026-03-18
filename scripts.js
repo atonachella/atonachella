581d3c895011aa85c132819dd42f3116ff5202d0
Adam Tonachella <adam.tonachella@gmail.com>
2026-03-18 14:34:24 -0700

    Removed Pop up JS

--- scripts.js
+++ scripts.js
@@ -2,12 +2,6 @@ console.log(`Basic Testing`);
 
 //  Display your notes here
 
-let county;
-
-county = window.prompt("Are you a Sonoma County Resident");
-
-console.log(county);
-
 const audio = document.getElementById("audioclip");
 
 audio.addEventListener('playing', (event) => {

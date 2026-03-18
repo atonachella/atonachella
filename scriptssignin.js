d4f2875d7509324a42caed9415fbb1241b02c671
Adam Tonachella <adam.tonachella@gmail.com>
2026-03-18 15:20:55 -0700

    JavaScript on Signin page AT

--- scriptssignin.js
+++ scriptssignin.js
@@ -1,3 +1,19 @@
 console.log(`Basic Testing for Sign In`);
 
 //  Display your notes here
+
+let username;
+
+let password;
+
+document.getElementById("signinbutton").onclick = function(){
+username = document.getElementById("username").ariaValueMax;
+console.log(username);
+
+document.getElementById("signinbutton").onclick = function(){
+username = document.getElementById("password").ariaValueMax;
+console.log(password);
+
+
+
+}

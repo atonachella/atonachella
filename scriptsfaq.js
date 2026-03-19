7f3bd4e2cf99c6f109246ff693f8da4d7cb00123
Adam Tonachella <adam.tonachella@gmail.com>
2026-03-18 19:35:50 -0700

    Java Script Additions AT

--- scriptsfaq.js
+++ scriptsfaq.js
@@ -1,3 +1,6 @@
 console.log(`Basic Testing for FAQ Page`);
 
 //  Display your JS notes here this page is under construction
+
+var x = document.getElementById("constructionwarning");
+x.color = #00ff00;
